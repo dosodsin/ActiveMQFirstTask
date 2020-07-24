53543fcfac38ab60eb094c49569c175099ecdfea
Vladislav Bormotov <vladstep9393@gmail.com>
2020-07-24 09:17:37 +0300

    v2

--- src/main/java/MessageReceiver.java
+++ src/main/java/MessageReceiver.java
@@ -7,6 +7,10 @@ import java.io.*;
 public class MessageReceiver {
     private static String url= ActiveMQConnection.DEFAULT_BROKER_URL;
     private static String queueName = "MESSAGE_QUEUE";
+    private static final int MODE_ACKNOWLEDGE = Session.AUTO_ACKNOWLEDGE;
+    private static final int MODE_TRANSACTED = Session.SESSION_TRANSACTED;
+    private static final int MODE_DUPS_OK_ACKNOWLEDGE = Session.DUPS_OK_ACKNOWLEDGE;
+    private static final int MODE_CLIENT_ACKNOWLEDGE = Session.CLIENT_ACKNOWLEDGE;
 
     public static void main(String[] args) throws JMSException, IOException {
         File file=new File("src/NonPersistentFinalFile.txt");
@@ -16,7 +20,7 @@ public class MessageReceiver {
         ConnectionFactory connectionFactory=new ActiveMQConnectionFactory(url);
         Connection connection=connectionFactory.createConnection();
 
-        Session session=connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        Session session=connection.createSession(false, MODE_ACKNOWLEDGE);
         Destination destination=session.createQueue(queueName);
         MessageConsumer consumer=session.createConsumer(destination);
         connection.start();
